ef6fcdb92b3632c2387fcdab39670861e0d90084
joaopbueno <mrjoao16@gmail.com>
2025-01-13 18:11:41 -0300

    Initial Commit

---
+++ TinderClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinderClone
+//
+//  Created by João Bueno on 13/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by João Bueno on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
