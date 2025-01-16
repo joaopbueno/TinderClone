c1c182e7c0c8e4b2aca314a2b7f6d4ef982be578
joaopbueno <mrjoao16@gmail.com>
2025-01-16 11:29:02 -0300

    Ajusteste de tela e animacao dos cards

--- TinderClone/Core/Cards/ViewModel/CardsViewModel.swift
+++ TinderClone/Core/Cards/ViewModel/CardsViewModel.swift
@@ -26,7 +26,10 @@ class CardsViewModel: ObservableObject {
     }
     
     func removeCard(_ card: CardModel) {
-        guard let index = cardModel.firstIndex(where: { $0.id == card.id}) else { return }
-        cardModel.remove(at: index)
+        Task {
+            try await Task.sleep(nanoseconds: 500_000_000)
+            guard let index = cardModel.firstIndex(where: { $0.id == card.id}) else { return }
+            cardModel.remove(at: index)
+        }
     }
 }
