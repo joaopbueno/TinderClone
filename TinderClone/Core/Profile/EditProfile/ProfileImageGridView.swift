b03b1c3f8fdb61025a89569595306d8144ff1fc3
joaopbueno <mrjoao16@gmail.com>
2025-01-21 11:10:06 -0300

    Edição de perfil

---
+++ TinderClone/Core/Profile/EditProfile/ProfileImageGridView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileImageGridView.swift
+//  TinderClone
+//
+//  Created by João Bueno on 21/01/25.
+//
+
+import SwiftUI
+
+struct ProfileImageGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileImageGridView()
+}
